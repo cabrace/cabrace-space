38adf8f786dfa592d1742164aa959b1942f6b7db
cabrace <christopher.a.brace@gmail.com>
2022-10-02 13:31:33 -0400

    Added stores

---
+++ src/lib/stores/blog.js
@@ -0,0 +1,16 @@
+import { writable } from 'svelte/store';
+
+
+export const checkedCategories = writable([]);
+export const posts_store = writable([]);
+
+export const categoryList = writable([
+    {name:"Svelte", checked: false},
+    {name:"Nginx", checked: false},
+    {name:"NextJS", checked: false},
+    {name:"Ruby", checked: false},
+    {name:"Python", checked: false},
+    {name:"General", checked: false},
+    {name:"Thoughts", checked: false},
+    {name:"Javascript", checked: false}
+  ]);

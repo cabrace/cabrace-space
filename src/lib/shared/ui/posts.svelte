a84e2e0c51320eddf21544c5a2d912f9bb0692da
cabrace <christopher.a.brace@gmail.com>
2022-10-19 23:36:21 -0400

    Added sample posts directory to be pulled form GH as API

---
+++ src/lib/shared/ui/posts.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	/* import { EyeIcon } from '@rgossiaux/svelte-heroicons/outline' */
+/*  */
+	/* import Transition from '$lib/shared/transition/index.svelte' */
+	/* import { views } from '$lib/stores/views' */
+	import type { PostType } from '$lib/types'
+
+	export let posts: PostType[]
+</script>
